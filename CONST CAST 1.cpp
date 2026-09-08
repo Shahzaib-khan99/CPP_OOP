73bf90cfd628b5a7e5685134dc7796e9a158e81f
Shahzaib Khan <aikingshahzaib0011277711@gmail.com>
2026-09-08 20:27:32 +0500

    Refactor student class and main function

--- CONST CAST 1.cpp
+++ CONST CAST 1.cpp
@@ -6,7 +6,7 @@ class student
 private: 
 	int roll;    
 public:   
-	student(int r):roll(r) {}
+	student(int r):roll(r) {} 
 	// A const function that changes roll with the help of const_cast 
 	void fun() const 
 	{  
